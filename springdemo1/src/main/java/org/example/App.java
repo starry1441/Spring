f5c6c3500e2d8e37c7d4b8eabe25a7f2d10daa6f
starry1441 <3363654551@qq.com>
2021-06-21 14:06:15 +0800

    2021_6_21

--- springdemo1/src/main/java/org/example/App.java
+++ springdemo1/src/main/java/org/example/App.java
@@ -1,8 +1,12 @@
 package org.example;
 
+import org.example.config.MyApplication;
 import org.example.controller.LoginController;
+import org.example.model.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.beans.Introspector;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,10 +19,25 @@ public class App {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        LoginController loginController = context.getBean(LoginController.class);
+        loginController.sayHi();
+
+//        MyApplication application = context.getBean(MyApplication.class);
+//        System.out.println(application);
+
+//        User user = (User)context.getBean("user1");
+//        System.out.println(user);
+//
+//        LoginController loginController = context.getBean(LoginController.class);
+//
+//        User user2 = context.getBean("user2",User.class);
+//        System.out.println(user2);
 //        LoginController loginController = (LoginController) context.getBean("loginController");
 //        System.out.println(loginController);
-        LoginController loginController = context.getBean(LoginController.class);
-        System.out.println(loginController);
+//        LoginController loginController = context.getBean(LoginController.class);
+//        System.out.println(loginController);
+//        System.out.println(Introspector.decapitalize("LoginController"));
         //关闭容器
         ((ClassPathXmlApplicationContext)context).close();
     }
---
+++ springdemo1/src/main/java/org/example/config/MyApplication.java
@@ -0,0 +1,16 @@
+package org.example.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: starry
+ * Date: 2021 -06 -21
+ * Time: 9:48
+ */
+
+@Configuration
+public class MyApplication {
+}
